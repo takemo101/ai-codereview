9d7bbb57c413888e648765b6a31bca7e2b1330cb
takemo101 <takemo101@gmail.com>
2025-01-18 12:59:24 +0900

    [refactor] 環境変数のプレフィクス指定オプションをデフォルト引数に変更

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
 type PrintEnvsOptions = {
-  // 環墿変数のプレフィクスの指定
+  /**
+   * 環境変数名の接頭辞
+   * この接頭辞で始まる環墩変数のみを出力する
+   */
   prefix?: string;
 };
 
-const printEnvs = async (options: PrintEnvsOptions) => {
+const printEnvs = async (options: PrintEnvsOptions = {}) => {
   const command = new Deno.Command("printenv");
   const { stdout } = await command.output();
   const decoded = new TextDecoder().decode(stdout);
@@ -23,5 +26,7 @@ const printEnvs = async (options: PrintEnvsOptions) => {
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  await printEnvs();
+  await printEnvs({
+    prefix: "VSCODE_GIT_ASKPASS",
+  });
 }
